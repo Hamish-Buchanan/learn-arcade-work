4a7ea14bae96c8bb56727a4d4ed4f9d7b2a5afcf
Hamish-Buchanan <orangeowl228.gaming@gmail.com>
2021-03-28 21:09:50 +1200

    First prototype

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -1,8 +1,10 @@
 """
-The Sword Theif's Journey is a text based adventure game where the player makes their way to the rebel base. The player
+The Sword Thief's Journey is a text based adventure game where the player makes their way to the rebel base. The player
 must stay ahead of the royal guard who, if they catch up, will kill the player. They also must manage their resources so
 as not to succumb to upkeep issues. They will also have to fight off chance encounters along the way.
 """
+
+import random
 
 def get_valid_string_input(prompt):
     valid = False
@@ -82,7 +84,94 @@ def steal_sword_start():
     print()
     return player_name
 
+def hunger_check():
+    if hunger == 0:
+        print("You feel full")
+    elif hunger <= 3:
+        print("You're peckish")
+    elif hunger <= 5:
+        print("You're hungry")
+    elif hunger <= 7:
+        print("You're famished")
+    else:
+        print("You're hunger is unbearable")
+
+def health_check():
+    if hp == 50:
+        print("You're unhurt")
+    elif hp >= 35:
+        print("You have a few scrapes and brusies")
+    elif hp >= 25:
+        print("You're pretty injured")
+    elif hp >= 10:
+        print("You're seriously hurt")
+    else:
+        print("You're on the verge of death")
+
+def thirst_check():
+    if thirst <= 3:
+        print("You're not thirsty")
+    elif thirst <= 5:
+        print("You're thirsty")
+    elif thirst <= 7:
+        print("You're parched")
+    else:
+        print("You are feeling very dehydrated")
+
+def horse_check():
+    if horse_tiredness == 0:
+        print("Your horse seems like it's raring to go!")
+    elif horse_tiredness <= 3:
+        print("Your horse is happy")
+    elif horse_tiredness <= 5:
+        print("Your horse seems a little tired")
+    elif horse_tiredness <= 7:
+        print("Your horse is panting heavily")
+    else:
+        print("Your horse looks extremely tired")
+
+def dead_check():
+    if hunger >= 10:
+        print("You collapse unconscious, never to awaken. You've starved to death")
+        return True
+    elif thirst >= 10:
+        print("Your throat as dry as you eye your horses saliva wistfully...")
+        print("You stumble towards the horse, unsteady on your feet. You stumble and fall over.")
+        print("Your consciousness fades. You've died of thirst")
+        return True
+    elif horse_tiredness >= 10:
+        print("Your horse, already moving slowly stumbles under your weight.")
+        print("You urge it on, tapping your spurs on the open wounds at it's side.")
+        print("It falls over, with you on it, and you tumble to the ground.")
+        print("You run away from the animal, from the guards. But without the horse you're too slow.")
+        print("You hear the sound of cantering hooves and turn, just in time to see an arrow whizz by.")
+        print("The next doesn't miss. You loo at it embedded in your chest. Another strikes your head.")
+        print("It all goes black")
+        return True
+    elif distance_traveled - guard_distance_traveled <= 0:
+        print("You hear shouts behind you. 'There he is!' the guards cry.")
+        print("You look, to see a muscular knight galloping towards you, axe in hand.")
+        print("You go to pull your blade, but an arrow strikes your shoulder and it falls to the ground with a clatter")
+        print("Terrified you turn back once more, to see the axe looming above your head. Then nothing.")
+    elif distance_traveled >= 250:
+        print("You slow down as a handsome young man waves you over.")
+        print(player_name + " you've made it! Come. We've much to discuss.")
+        print("And with that, the sword thief's journey ends and the legend of " + player_name + " begins")
+        return True
+    else :
+        if hunger > 7:
+            print("Your hunger is unbearable")
+        if thirst > 7:
+            print("You are feeling very dehydrated")
+        if hp < 10:
+            print("You're on the verge of death")
+        if horse_tiredness > 7:
+            print("Your horse looks extremely tired. It's skin is raw where your stirrups touch it.")
+        return False
+
 def day_actions():
+    global player_name, time, distance_traveled, guard_distance_traveled, thirst, hunger, horse_tiredness
+    global food_in_bag, drinks_in_bottle, hp, atk, defence
     print("It's " + str(time) + " O'Clock")
     print("What do you do?")
     print("A. Eat something.")
@@ -98,29 +187,247 @@ def day_actions():
         return True
 
     elif action_choice == 'a':
-        print("Add stuff here")
+        if hunger > 0 and food_in_bag > 0:
+            print("You eat some food from your bag")
+            food_in_bag -= 1
+            if hunger >= 5:
+                hunger -= 5
+            else:
+                hunger = 0
+            hunger_check()
+        elif not hunger > 0:
+            print("You're too full to eat anymore")
+        else:
+            print("You don't have any food left")
 
     elif action_choice == 'b':
-        print("Add stuff here")
+        if thirst > 0 and drinks_in_bottle > 0:
+            print("You drink from your bottle")
+            drinks_in_bottle -= 1
+            thirst = 0
+            thirst_check()
+        elif not thirst > 0:
+            print("You don't feel thirsty right now")
+        else:
+            print("You don't have any water left")
 
     elif action_choice == 'c':
-        print("Add stuff here")
+        num_hours = get_valid_number_input("For how many hours? ")
+        if time + num_hours > 24:
+            time = time + num_hours - 24
+        else:
+            time += num_hours
+        print("You push your horse to go as fast as it can!")
+        new_dist = 0
+        guard_dist = 0
+        for i in range(num_hours):
+            horse_tiredness += random.randint(1, 2)
+            new_dist += random.randint(7, 12)
+            guard_dist += random.randint(6, 10)
+            hunger += random.random()/4*3
+            thirst += random.random()
+        distance_traveled = distance_traveled + new_dist
+        guard_distance_traveled = guard_distance_traveled + guard_dist
+        if dead_check():
+            return True
+        print("You've traveled " + str(new_dist) + " miles")
+        print("The guards traveled " + str(guard_dist) + " miles")
+        if guard_distance_traveled <= 0:
+            print("The guards are far behind you")
+        else:
+            print("The guards seem to be " + str(distance_traveled - guard_distance_traveled) + " miles behind you")
 
     elif action_choice == 'd':
-        print("Add stuff here")
+        num_hours = get_valid_number_input("For how many hours? ")
+        if time + num_hours > 24:
+            time = time + num_hours - 24
+        else:
+            time += num_hours
+        print("You set off at a steady pace")
+        new_dist = 0
+        guard_dist = 0
+        for i in range(num_hours):
+            horse_tiredness += random.randint(1, 2)
+            new_dist += random.randint(5, 8)
+            guard_dist += random.randint(6, 10)
+            hunger += random.random() / 4 * 3
+            thirst += random.random()
+        distance_traveled = distance_traveled + new_dist
+        guard_distance_traveled = guard_distance_traveled + guard_dist
+        if dead_check():
+            return True
+        print("You've traveled " + str(new_dist) + " miles")
+        print("The guards traveled " + str(guard_dist) + " miles")
+        if guard_distance_traveled <= 0:
+            print("The guards are far behind you")
+        else:
+            print("The guards seem to be " + str(distance_traveled - guard_distance_traveled) + " miles behind you")
 
     elif action_choice == 'e':
-        print("Add stuff here")
+        num_hours = get_valid_number_input("For how many hours? ")
+        if time + num_hours > 24:
+            time = time + num_hours - 24
+        else:
+            time += num_hours
+        print("You tie up your horse in a field with lush grass and doze off")
+        guard_dist = 0
+        for i in range(num_hours):
+            horse_tiredness -= random.randint(2, 3)
+            guard_dist += random.randint(6, 10)
+            hp += random.randint(4,8)
+            hunger += random.random() / 3
+            thirst += random.random() / 2
+        if hp > 50 : hp = 50
+        guard_distance_traveled = guard_distance_traveled + guard_dist
+        if dead_check():
+            return True
+        horse_check()
+        print("The guards traveled " + str(guard_dist) + " miles")
+        if guard_distance_traveled <= 0:
+            print("The guards are far behind you")
+        else:
+            print("The guards seem to be " + str(distance_traveled - guard_distance_traveled) + " miles behind you")
 
     elif action_choice == 'f':
-        print("Add stuff here")
+        print("You do a stock take : ")
+        health_check()
+        hunger_check()
+        thirst_check()
+        horse_check()
+        print("You have " + str(food_in_bag) + " pieces of food in your bag")
+        print("You have " + str(drinks_in_bottle) + " drinks left in your bag")
+        print("You have traveled around " + str(distance_traveled) + " miles")
+        if guard_distance_traveled <= 0:
+            print("The guards are far behind you")
+        else:
+            print("The guards seem to be " + str(distance_traveled - guard_distance_traveled) + " miles behind you")
+    if time >= 20:
+        print("The sun has set and the moon rises into the sky. The guards have turned in for the night.")
+        print("You hear the eerie cries of monsters in the dark. Traveling will be more dangerous now.")
 
+def night_actions():
+    global player_name, time, distance_traveled, guard_distance_traveled, thirst, hunger, horse_tiredness
+    global food_in_bag, drinks_in_bottle, hp, atk, defence
+    print("It's " + str(time) + " O'Clock")
+    print("What do you do?")
+    print("A. Eat something.")
+    print("B. Have a drink.")
+    print("C. Move ahead full speed")
+    print("D. Move ahead medium speed")
+    print("E. Sleep for the night")
+    print("F. Status check")
+    print("Q. Give up")
+    action_choice = get_action_input("What do you want to do? ", ["a", "b", "c", "d", "e", "f", "q"])
+    if action_choice == 'q':
+        print("You give up and enjoy the few hours you have before the guards catch up. They kill you.")
+        return True
+
+    elif action_choice == 'a':
+        if hunger > 0 and food_in_bag > 0:
+            print("You eat some food from your bag")
+            food_in_bag -= 1
+            if hunger >= 5:
+                hunger -= 5
+            else:
+                hunger = 0
+            hunger_check()
+        elif not hunger > 0:
+            print("You're too full to eat anymore")
+        else:
+            print("You don't have any food left")
+
+    elif action_choice == 'b':
+        if thirst > 0 and drinks_in_bottle > 0:
+            print("You drink from your bottle")
+            drinks_in_bottle -= 1
+            thirst = 0
+            thirst_check()
+        elif not thirst > 0:
+            print("You don't feel thirsty right now")
+        else:
+            print("You don't have any water left")
+
+    elif action_choice == 'c':
+        num_hours = get_valid_number_input("For how many hours? ")
+        if time + num_hours > 24:
+            time = time + num_hours - 24
+        else:
+            time += num_hours
+        print("You push your horse to go as fast as it can!")
+        new_dist = 0
+        for i in range(num_hours):
+            horse_tiredness += random.randint(1, 2)
+            new_dist += random.randint(6, 10)
+            hunger += random.random()/4*3
+            thirst += random.random()
+        distance_traveled = distance_traveled + new_dist
+        if dead_check():
+            return True
+        print("You've traveled " + str(new_dist) + " miles")
+        print("The guards slept")
+        if guard_distance_traveled <= 0:
+            print("The guards are far behind you")
+        else:
+            print("The guards seem to be " + str(distance_traveled - guard_distance_traveled) + " miles behind you")
+
+    elif action_choice == 'd':
+        num_hours = get_valid_number_input("For how many hours? ")
+        if time + num_hours > 24:
+            time = time + num_hours - 24
+        else:
+            time += num_hours
+        print("You set off at a steady pace")
+        new_dist = 0
+        for i in range(num_hours):
+            horse_tiredness += random.randint(1, 2)
+            new_dist += random.randint(3, 7)
+            hunger += random.random() / 4 * 3
+            thirst += random.random()
+        distance_traveled = distance_traveled + new_dist
+        if dead_check():
+            return True
+        print("You've traveled " + str(new_dist) + " miles")
+        print("The guards slept")
+        if guard_distance_traveled <= 0:
+            print("The guards are far behind you")
+        else:
+            print("The guards seem to be " + str(distance_traveled - guard_distance_traveled) + " miles behind you")
+
+    elif action_choice == 'e':
+        if time >= 19:
+            time_slept = (time-24)*-1 + 7
+        else:
+            time_slept = 7-time
+        time = 7
+        horse_tiredness -= 1.5*time_slept
+        if horse_tiredness < 0: horse_tiredness = 0
+        hunger += random.random() / 4 * 3
+        thirst += random.random()
+        hp += 3*time_slept
+        if hp > 50: hp = 50
+        print("You set up camp for the night. You tie up your horse and nod off.")
+        if dead_check():
+            return True
+
+    elif action_choice == 'f':
+        print("You do a stock take : ")
+        health_check()
+        hunger_check()
+        thirst_check()
+        horse_check()
+        print("You have " + str(food_in_bag) + " pieces of food in your bag")
+        print("You have " + str(drinks_in_bottle) + " drinks left in your bag")
+        print("You have traveled around " + str(distance_traveled) + " miles")
+        if guard_distance_traveled <= 0:
+            print("The guards are far behind you")
+        else:
+            print("The guards seem to be " + str(distance_traveled - guard_distance_traveled) + " miles behind you")
 
 
 if __name__ == "__main__" :
     print_intro()
-    global player_name, time, distance_traveled, guard_distance_traveled, thirst, hunger, horse_tiredness, food
-    global food_in_bag, drinks_in_bottle
+    global player_name, time, distance_traveled, guard_distance_traveled, thirst, hunger, horse_tiredness
+    global food_in_bag, drinks_in_bottle, hp, atk, defence
     player_name = steal_sword_start()
     time = 8
     thirst = 0
@@ -130,9 +437,13 @@ if __name__ == "__main__" :
     drinks_in_bottle = 3
     distance_traveled = 0
     guard_distance_traveled = -25
-
+    hp = 50
+    atk = 5
+    defence = 8
     dead = False
     while not dead:
         if time <= 19 and time >= 7 :
             dead = day_actions()
+        else:
+            dead = night_actions()
         print()
